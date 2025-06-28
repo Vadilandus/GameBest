1da85c6ada65c6e553d069e2ddc4c1538156a89f
Vadim <antonuk_proo@mail.ru>
2025-06-28 23:41:20 +0300

    Правильная версия

---
+++ PetGameAirBattle/Models/BaseObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PetGameAirBattle.Models
+{
+    abstract class BaseObject
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public UIElement? UiElement { get; protected set; }
+        public abstract void Draw(Canvas canvas);
+    }
+}
---
+++ PetGameAirBattle/Models/PlayerObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PetGameAirBattle.Models
+{
+    class PlayerObject : BaseObject
+    {
+        public override void Draw(Canvas canvas)
+        {
+            if (UiElement == null)
+            {
+                // Создаем элемент
+                var rect = new Rectangle
+                {
+                    Width = Width,
+                    Height = Height,
+                    Fill = Brushes.Blue
+                };
+                UiElement = rect;
+                canvas.Children.Add(rect);
+            }
+
+            // Обновляем позицию
+            Canvas.SetLeft(UiElement, X);
+            Canvas.SetTop(UiElement, Y);
+        }
+    }
+}
